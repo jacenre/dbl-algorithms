493071409e8c61799e1e5232986d804a976687d6
Jore Booy <jorebooy1@gmail.com>
2020-02-18 14:39:44 +0100

    Added ReverseFitSolverTest

---
+++ test/ReverseFitSolverTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Testing class using {@link ReverseFitSolver}
+ */
+@DisplayName("Reverse-Fit Solver")
+public class ReverseFitSolverTest extends AbstractPackingSolverTest {
+
+    @Override
+    AbstractSolver getSolver() {
+        return new ReverseFitSolver();
+    }
+
+    @Override
+    List<AbstractBinGenerator> getGenerators() {
+        return Arrays.asList(new FixedOptimalBinGenerator(), new FixedRotatingOptimalBinGenerator());
+    }
+}
