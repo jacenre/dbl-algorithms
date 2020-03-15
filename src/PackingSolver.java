9912934f8945230a8bfbe94f22b8fbfff1a7ef6d
Jore Booy <jorebooy1@gmail.com>
2020-03-15 16:47:47 +0100

    Small output change.

--- src/PackingSolver.java
+++ src/PackingSolver.java
@@ -19,7 +19,7 @@ public class PackingSolver {
         compoundSolver.addSolver(new SimpleTopLeftSolver());
 
         Solution solution = compoundSolver.getSolution(params);
-        System.err.println(solution.solvedBy);
+        if (Util.debug) System.err.println(solution.solvedBy);
         Output.output(solution.parameters, inputOrder);
     }
 
