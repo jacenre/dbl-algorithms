754f4b78d8c57b00edd425a41390b07ad2c43d52
Marijn Ritzen <m.m.ritzen@student.tue.nl>
2020-03-27 11:41:12 +0100

    Small start to the documentation and specific methods that we're gonna need for skyline

--- src/SkylineSolver.java
+++ src/SkylineSolver.java
@@ -9,10 +9,7 @@ import java.util.Vector;
  */
 public class SkylineSolver extends AbstractSolver {
 
-    int SPREAD_CONSTRAINT = 100;
-//    // ArrayList representing the skyline, starting of with a segment corresponding to the height of the sheet.
-//    ArrayList<Segment> skyline = new ArrayList<>(); // Important to keep this ordered
-//        skyline.add(new Segment(new Point(0, 0), new Point(0, parameters.height)));
+    Solution globalSolution;
 
     // Algorithm 2 in the paper
     @Override
@@ -25,7 +22,7 @@ public class SkylineSolver extends AbstractSolver {
         while(/*time limit not reached and */ lowerBound != upperBound) {
             int tempLowerBound = lowerBound;
             while (tempLowerBound < upperBound) {
-                int width = (int)((tempLowerBound + upperBound) / 2);
+                int width = ((tempLowerBound + upperBound) / 2);
                 if (solve(width, iter)) {
                     /* record this solution */
                     upperBound = width;
@@ -38,9 +35,16 @@ public class SkylineSolver extends AbstractSolver {
                 upperBound = (int)(upperBound * 1.1);
             iter *= 2;
         }
-        return new Solution(parameters);
+        return globalSolution;
     }
 
+    /**
+     * Gives the lowerbound for the binary search for a suitable width of the square area that is used in the second
+     * algorithm.
+     *
+     * @param parameters
+     * @return The lowerbound, which is dependent on rotations variant and the given rectangles
+     */
     int getLowerBound(Parameters parameters) {
         int totalArea = 0;
         int LB2 = 0;
@@ -61,35 +65,74 @@ public class SkylineSolver extends AbstractSolver {
         return Math.max(LB1, LB2 + (int)Math.ceil(LB3 / 2));
     }
 
-    // Algorithm 1 in the paper
+    /**
+     * Handles a Tabu search for a good sequence of rectangles and good maximum spread.
+     *
+     * @param W The width that has been supplied by the pack method
+     * @param iter The iterations variant that has also been supplied by the pack method
+     * @returns a boolean signalling if a solution could be found with the given attributes
+     */
     boolean solve(int W, int iter) {
 
     }
 
-    ArrayList<Point> getCandidatePositions(ArrayList<Segment> skyline) {
-        ArrayList<Point> points = new ArrayList<>();
-        points.add(skyline.get(0).start);
-        points.add(skyline.get(skyline.size() - 1).end);
+    /**
+     * Goes through the heurstics and places the sequence of rectangles in the box while maintaining a skyline view of
+     * the whole ordeal. Returns if a solution was able to be found with the given maximumSpread and width.
+     *
+     * @param sequence The sequence of rectangles, which can be very different according to different sorting and
+     *                 the random permutations made by the tabu search algorithm
+     * @param skyline The skyline on which to work
+     * @param width The given width to which to adhere
+     * @param maximumSpread The given maximumSpread to which to adhere
+     * @return true or false whether the heuristic was able to pack all the rectangles given the restrictions
+     */
+    boolean heuristicPacking(ArrayList<Rectangle> sequence, ArrayList<Segment> skyline, int width, int maximumSpread) {
+        for (Segment segment : skyline) {
+            for (Rectangle rectangle : sequence) {
 
-        if (skyline.size() == 1) {
-            return points;
+            }
         }
+    }
 
-        // At least two segments
+    /**
+     * A class which is necessary to keep track of the relative position of the points to the segment of which they are part
+     */
+    class SegPoint extends java.awt.Point {
+        boolean start;
 
-        // Check edge cases
-        if (skyline.get(0).end.x < skyline.get(1).start.x) {
-            points.add(skyline.get(0).end);
+        public SegPoint (boolean start) {
+            super();
+            this.start = start;
         }
-        if (skyline.get(skyline.size() - 1).start.x < skyline.get(skyline.size() - 2).end.x) {
-            points.add(skyline.get(skyline.size() - 1).start);
+    }
+
+    /**
+     * Computes all the candidate positions (where a new rectangle could be placed), given a skyline and width
+     *
+     * @param skyline The skyline to be used
+     * @param width A width to which the skyline needs to be adhered to
+     * @returns the list of points on which new rectangles can be placed
+     */
+    ArrayList<SegPoint> getCandidatePositions(ArrayList<Segment> skyline, int width) {
+        ArrayList<SegPoint> points = new ArrayList<>();
+        if (skyline.get(0).start.x < width) {
+            points.add(skyline.get(0).start);
         }
 
         // Check all the middle ones
-        for (int i = 1; i < skyline.size() - 1; i++) {
-
+        for (int i = 0; i < skyline.size(); i++) {
+            if (i != 0 && skyline.get(i).start.x < skyline.get(i-1).end.x) {
+                points.add(skyline.get(i).start);
+            }
+            if (i != skyline.size() - 1 && skyline.get(i).end.x < skyline.get(i+1).start.x) {
+                points.add(skyline.get(i).end);
+            }
         }
 
+        if (skyline.get(skyline.size() - 1).end.x < width) {
+            points.add(skyline.get(skyline.size() - 1).end);
+        }
         return points;
     }
 
@@ -102,10 +145,10 @@ public class SkylineSolver extends AbstractSolver {
      */
     class Segment {
 
-        Point start = null;
-        Point end = null;
+        SegPoint start = null;
+        SegPoint end = null;
 
-        Segment(Point start, Point end) {
+        Segment(SegPoint start, SegPoint end) {
             this.start = start;
             this.end = end;
         }
@@ -155,6 +198,13 @@ public class SkylineSolver extends AbstractSolver {
         }
     }
 
+    /**
+     * Returns the most left segment in the skyline. Usefull for checking if a newly placed rectangles breaks the
+     * spread constraint heuristic.
+     *
+     * @param skyline The skyline to be used
+     * @returns An integer of the x position of the most left segment
+     */
     int getMostLeftPoint(ArrayList<Segment> skyline) {
         int mostLeftPoint = Integer.MAX_VALUE;
         for (Segment segment : skyline) {
